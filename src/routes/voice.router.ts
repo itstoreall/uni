cb1ec992796dc9c887676ad942da913a2fe07347
Serhii Stanislav <serhiistanislav@gmail.com>
2024-05-04 20:11:24 +0300

    ADD | pre-commit

--- src/routes/voice.router.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { generateSpeechFromText } from "../controllers/voice.controller";
-
-const router = express.Router();
-
-router.route("/generate-speech-from-text").post(generateSpeechFromText);
-
-export default router;
